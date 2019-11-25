82904ead3f36f063fd0e0d4224d6c0f39ec3cb3f
zackerymarkusrayspears <zackery.spears97@gmail.com>
2019-11-25 12:15:07 -0600

    Added code for the Node server to get it up and running using Express and Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,45 @@
+//Mongoose - allows us to set up schemas for the data we store in our MongoDB datebase. We will also set up our datebase connection using Mongoose.
 const mongoose = require('mongoose');
+//Express - framework for NodeJS. Handles our routing and connection info (port, etc.).
 const express = require('express');
+//Morgan - Handles logging stuff for databases.
 const logger = require('morgan');
+//Body-Parser = Makes sure the body of our request is formatted correctly (in the case, we'll use JSON).
 const bodyParser = require('body-parser');
+
+//Import the getSecret function for the secrets.js file
+const getSecret = require('./secrets');
+
+//Constant to hold the port that we are going to use to connect 
+const API_PORT = 3001;
+
+//Creat an Express app that will run our Node server and route our request
+const app = express();
+
+//Create an Express router which actually handles the routing 
+const router = express.Router();
+
+//Mongoose to set up a connection to the database
+mongoose.connect(getSecret('dbUrl'), {useNewUrlParser: true, useFindAndModify: false});
+
+//Reference to our database connection
+let db = mongoose.connection;
+
+//Use the database connection to print oit an error if one occurs when we try to connect to the database
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+//Configure body-parser and morgan 
+app.use(bodyParser.urlencoded({extend: false})); //Used for parsing data formatted in tne x-www-form-urlencoded format
+app.use(bodyParser.json()); //Used for parsing the data formatted in the JSON format
+app.use(logger('dev')); 
+
+//Default rout that will run whenever we first connect to the server 
+router.get('/', (req, res) =>{
+    res.json({message: 'HELLO WORLD'});
+});
+
+//Tell Express to use a certain path and to use the router we set up 
+app.use('/api', router);
+
+//Tell Express to listen for requests on the appropriate port
+app.listen(API_PORT, () => console.log('LISTENING ON PORT' + API_PORT));
